5f8bd95ab94328b512b641cf03a601357b35341a
Amy Yan <ayanamydev@gmail.com>
2024-04-01 10:30:31 +1100

    wip

--- src/app.rs
+++ src/app.rs
@@ -4,7 +4,7 @@ use crate::components::ask::{Ask, AskCard};
 use crate::components::live_version_select_options::LiveVersionSelectOptions;
 
 use js_sys::wasm_bindgen::JsCast;
-use web_sys::HtmlSelectElement;
+use web_sys::{FileList, HtmlInputElement, HtmlSelectElement};
 use yew::prelude::*;
 use altc::util::{self, LiveVersion};
 
@@ -41,39 +41,35 @@ pub fn app() -> Html {
         Ok::<Ask, DeErrorWrapper>(new_ask)
     }).collect::<Vec<_>>();
 
-    let process_files = {
-        let files = asks.clone();
+    let process_uploaded_files = {
+        let asks = asks.clone();
         let default_version_to = default_version_to.clone();
-        Callback::from(move |event: web_sys::DragEvent| {
-            event.prevent_default();
-            let files = files.clone();
+        Callback::from(move |filelist: FileList| {
+            let asks = asks.clone();
             let default_version_to = default_version_to.clone();
             wasm_bindgen_futures::spawn_local(async move {
-                let files = files.clone();
-                let uploaded_files = event.data_transfer().unwrap().files();
-                if let Some(filelist) = uploaded_files {
-                    let uploaded_files = js_sys::try_iter(&filelist)
-                        .unwrap()
-                        .unwrap()
-                        .map(|v| web_sys::File::from(v.unwrap()))
-                        .map(web_sys::File::from);
-                    let mut new_files = (*files).clone();
-                    for uploaded_file in uploaded_files {
-                        let file_promise: js_sys::Promise = uploaded_file.text();
-                        let file_text_js_value = wasm_bindgen_futures::JsFuture::from(file_promise)
-                            .await
-                            .unwrap();
-                        let file_text = file_text_js_value.as_string().unwrap();
-                        let default_version_to = default_version_to.clone();
-                        new_files.push(Ask {
-                            name: uploaded_file.name(),
-                            version: util::get_live_version(&file_text).unwrap_or(util::LiveVersion::Live12),
-                            contents: file_text,
-                            to_version: Some(*default_version_to)
-                        });
-                    }
-                    files.set(new_files);
+            let asks = asks.clone();
+                let uploaded_files = js_sys::try_iter(&filelist)
+                    .unwrap()
+                    .unwrap()
+                    .map(|v| web_sys::File::from(v.unwrap()))
+                    .map(web_sys::File::from);
+                let mut new_asks = (*asks).clone();
+                for uploaded_file in uploaded_files {
+                    let file_promise: js_sys::Promise = uploaded_file.text();
+                    let file_text_js_value = wasm_bindgen_futures::JsFuture::from(file_promise)
+                        .await
+                        .unwrap();
+                    let file_text = file_text_js_value.as_string().unwrap();
+                    let default_version_to = default_version_to.clone();
+                    new_asks.push(Ask {
+                        name: uploaded_file.name(),
+                        version: util::get_live_version(&file_text).unwrap_or(util::LiveVersion::Live12),
+                        contents: file_text,
+                        to_version: Some(*default_version_to)
+                    });
                 }
+                asks.set(new_asks);
             });
         })
     };
@@ -110,13 +106,33 @@ pub fn app() -> Html {
                 <div class="flex flex-col rounded-lg border bg-card text-card-foreground shadow-sm flex-1">
                     <div class="border-b p-6 flex justify-between">
                         { "Drag .ask files here" }
-                        <button>
+                        <input
+                            id="uploader"
+                            type="file"
+                            multiple={true}
+                            class="hidden" 
+                            onchange={{
+                                let process_uploaded_files = process_uploaded_files.clone();
+                                Callback::from(move |event: Event| {
+                                    let input: HtmlInputElement = event.target().unwrap().dyn_into().unwrap();
+                                    if let Some(uploaded_files) = input.files() {
+                                        process_uploaded_files.emit(uploaded_files);
+                                    }
+                                })
+                            }}
+                        />
+                        <label tabindex="0" class="cursor-pointer" for="uploader">
                             <svg width="15" height="15" viewBox="0 0 15 15" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M7.81825 1.18188C7.64251 1.00615 7.35759 1.00615 7.18185 1.18188L4.18185 4.18188C4.00611 4.35762 4.00611 4.64254 4.18185 4.81828C4.35759 4.99401 4.64251 4.99401 4.81825 4.81828L7.05005 2.58648V9.49996C7.05005 9.74849 7.25152 9.94996 7.50005 9.94996C7.74858 9.94996 7.95005 9.74849 7.95005 9.49996V2.58648L10.1819 4.81828C10.3576 4.99401 10.6425 4.99401 10.8182 4.81828C10.994 4.64254 10.994 4.35762 10.8182 4.18188L7.81825 1.18188ZM2.5 9.99997C2.77614 9.99997 3 10.2238 3 10.5V12C3 12.5538 3.44565 13 3.99635 13H11.0012C11.5529 13 12 12.5528 12 12V10.5C12 10.2238 12.2239 9.99997 12.5 9.99997C12.7761 9.99997 13 10.2238 13 10.5V12C13 13.104 12.1062 14 11.0012 14H3.99635C2.89019 14 2 13.103 2 12V10.5C2 10.2238 2.22386 9.99997 2.5 9.99997Z" fill="currentColor" fill-rule="evenodd" clip-rule="evenodd"></path></svg>
-                        </button>
+                        </label>
                     </div>
                     <div
                         class="p-6 flex-1 space-y-3 overflow-y-auto"
-                        ondrop={process_files}
+                        ondrop={Callback::from(move |event: DragEvent| {
+                            event.prevent_default();
+                            if let Some(uploaded_files) = event.data_transfer().unwrap().files() {
+                                process_uploaded_files.emit(uploaded_files);
+                            }
+                        })}
                         ondragover={Callback::from(|event: DragEvent| {
                             event.prevent_default();
                         })}
